91419e6e5ddc19ac8a90641d8806fcd6e345dd27
Tharana Hansaja <31825168+Tharana@users.noreply.github.com>
2020-10-05 22:16:36 +0530

    Update script.js

--- tar/script.js
+++ tar/script.js
@@ -15,7 +15,7 @@ $('.four-oh-four-form').on('submit', function(e){
   var val = $(this).children($('.404-input')).val().toLowerCase();
   var href;
 
-	 if (val === 'Contact'){
+	 if (val === 'contact'){
     showKittens();
   }else {
     resetForm();
